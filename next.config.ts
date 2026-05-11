f1112818643a42cecc59c375e0690778e84aacab
Akshaya Nakhate <akshayanakhate123@gmail.com>
2026-05-12 00:31:26 +0530

    Setup GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
+const repoName = "my-portfolio";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
