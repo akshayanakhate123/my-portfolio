24a566a09d0a3d63e1927b19bae2a9b5d6ef4bb4
Akshaya Nakhate <akshayanakhate123@gmail.com>
2026-05-12 00:56:36 +0530

    Configure basePath and assetPrefix for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 
+const repoName = "my-portfolio";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+
   eslint: {
   ignoreDuringBuilds: true,
 },
